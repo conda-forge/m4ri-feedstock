9b924a2197c3ddf5fef5047d4b66cc7b15866ab9
Isuru Fernando <isuruf@gmail.com>
2018-09-16 01:29:17 -0500

    Add windows support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
-export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PREFIX/lib"
-export CFLAGS="-O2 -g -fPIC $CFLAGS"
-
 chmod +x configure
 
 ./configure --prefix=$PREFIX --libdir=$PREFIX/lib --disable-sse2
@@ -12,3 +7,34 @@ chmod +x configure
 make
 make check
 make install
+
+
+case `uname` in
+    Darwin|Linux)
+        export CFLAGS="-O3 -g -fPIC $CFLAGS"
+        ./configure --prefix="$PREFIX" --libdir="$PREFIX"/lib --disable-sse2
+        ;;
+    MINGW*)
+        export PATH="$PREFIX/Library/bin:$BUILD_PREFIX/Library/bin:$PATH"
+        export CC=clang-cl
+        export RANLIB=llvm-ranlib
+        export AS=llvm-as
+        export AR=llvm-ar
+        export LD=lld-link
+        export CFLAGS="-MD -I$PREFIX/Library/include -O3 -Dsrandom=srand -Drandom=rand"
+        export LDFLAGS="$LDFLAGS -L$PREFIX/Library/lib"
+        ./configure --prefix="$PREFIX/Library" --libdir="$PREFIX/Library/lib" --disable-sse2
+        ;;
+esac
+
+make -j${CPU_COUNT}
+make check -j${CPU_COUNT}
+make install
+
+PROJECT=m4ri
+if [[ `uname` == MINGW* ]]; then
+    LIBRARY_LIB=$PREFIX/Library/lib
+    mv "${LIBRARY_LIB}/${PROJECT}.lib" "${LIBRARY_LIB}/${PROJECT}_static.lib"
+    mv "${LIBRARY_LIB}/${PROJECT}.dll.lib" "${LIBRARY_LIB}/${PROJECT}.lib"
+fi
+
