0ca55448c7935821088e4143fb0b9ade6b2274ac
Marcelo Duarte Trevisani <marceloduartetrevisani@gmail.com>
2018-10-08 08:35:16 +0200

    Migration

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,9 +5,15 @@ export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PREFIX/lib"
 export CFLAGS="-O2 -g -fPIC $CFLAGS"
 
+autoreconf --install
+
 chmod +x configure
 
-./configure --prefix=$PREFIX --libdir=$PREFIX/lib --disable-sse2
+./configure --prefix=$PREFIX \
+            --host=$HOST \
+            --build=$BUILD \
+            --libdir=$PREFIX/lib \
+            --disable-sse2
 
 make
 make check
