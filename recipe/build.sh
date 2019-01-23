b2d7757447a7306e87765f6559d2ffebf44aa654
Isuru Fernando <isuruf@gmail.com>
2019-01-22 19:01:04 -0600

    Use cl_wrapper.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,16 +10,17 @@ case `uname` in
         ./configure --prefix="$PREFIX" --libdir="$PREFIX"/lib --disable-sse2
         ;;
     MINGW*)
-        export PATH="$PREFIX/Library/bin:$BUILD_PREFIX/Library/bin:$PATH"
-        export CC=clang-cl
+        export PATH="$PREFIX/Library/bin:$BUILD_PREFIX/Library/bin:$RECIPE_DIR:$PATH"
+        export CC=cl_wrapper.sh
         export RANLIB=llvm-ranlib
         export AS=llvm-as
         export AR=llvm-ar
         export LD=lld-link
-        export CFLAGS="-MD -I$PREFIX/Library/include -O3 -Dsrandom=srand -Drandom=rand"
+        export CFLAGS="-MD -I$PREFIX/Library/include -O2 -Dsrandom=srand -Drandom=rand"
         export LDFLAGS="$LDFLAGS -L$PREFIX/Library/lib"
         sed -i "s/-Wl,-DLL,-IMPLIB/-link -DLL -IMPLIB/g" configure
         sed -i "s/#include <unistd.h>//g" m4ri/djb.c
+        cp "$PREFIX/Library/lib/libpng.lib" "$PREFIX/Library/lib/png.lib"
         ./configure --prefix="$PREFIX/Library" --libdir="$PREFIX/Library/lib" --disable-sse2
         ;;
 esac
@@ -33,5 +34,6 @@ if [[ `uname` == MINGW* ]]; then
     LIBRARY_LIB=$PREFIX/Library/lib
     mv "${LIBRARY_LIB}/${PROJECT}.lib" "${LIBRARY_LIB}/${PROJECT}_static.lib"
     mv "${LIBRARY_LIB}/${PROJECT}.dll.lib" "${LIBRARY_LIB}/${PROJECT}.lib"
+    rm "${LIBRARY_LIB}/png.lib"
 fi
 
